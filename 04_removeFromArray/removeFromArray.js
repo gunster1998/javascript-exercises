d9440883d702ac22ffca743bf6823cd8c1f0b24f
Konstantin Dryagin <123456789+odin@users.noreply.github.com>
2024-10-07 15:38:59 +0300

    fix: Переделал код использовал inculeds and filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,26 @@
 const removeFromArray = function(array,...args) {
-     for (let argumentsNumber = 0; argumentsNumber < args.length; argumentsNumber++){
-          for(let i=0; i<array.length;i++){
-               if (array[i] === args[argumentsNumber]){
-                    array.splice((array.indexOf(args[argumentsNumber])),1);
-                    i--;
-               }
-          }
-     }
-     return array;
+     return array.filter(element => !args.includes(element))
 }
+
+
 
 
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//Предпоследняя  версия 
+// const removeFromArray = function(array,...args) {
+//      for (let argumentsNumber = 0; argumentsNumber < args.length; argumentsNumber++) { 
+//      if (array.includes(args[argumentsNumber])) {
+//           array = array.filter(arg => args[argumentsNumber] !== arg);
+//      }
+// }
+//      return array
+// }
+
+
+
 
 
 
@@ -25,3 +32,17 @@ module.exports = removeFromArray;
 //      array.splice(array.indexOf(argumentOne),1) ;
 // }
 // return array;
+
+
+// const removeFromArray = function(array,...args) {
+//      for (let argumentsNumber = 0; argumentsNumber < args.length; argumentsNumber++) { 
+//           for(let i=0; i < array.length;i++){
+//                if (array[i] === args[argumentsNumber]) {
+//                     array.splice((array.indexOf(args[argumentsNumber])),1);
+//                     i--;
+//                }
+//           }
+//      }
+//      return array;
+// }
+
