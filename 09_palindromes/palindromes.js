32cdbee5e7fe7a830b0f1ef9b9e2edf83599183f
Konstantin Dryagin <gunster98@gmail.com>
2024-11-07 18:27:37 +0300

    fix: 09 сделал через reverse

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,23 +1,27 @@
 const palindromes = function (string) {
-    const palindromeArray = string.trim().replace(/[!%$" ",.]/g, '').toLowerCase().split('');
-    const lengthPalindromeArray = palindromeArray.length
-    console.log(palindromeArray)
-    const halfArray = Math.floor(palindromeArray.length / 2);
-    const lastIndex = palindromeArray.length - 1
-
-        // console.log(palindromeArray,halfArray,lastIndex)
-        for (let i = 0; i < halfArray; i++ ){
-            if (palindromeArray[i] !== palindromeArray[lastIndex-i]){
-                return false
-            }
-        }
-    return true
-};
+    const palindromeArray = string.trim().replace(/[^a-z0-9]/ig, '').toLowerCase().split('');
+    const palidromReverse = [...palindromeArray].reverse();
+    console.log(palindromeArray === palidromReverse, palindromeArray,palidromReverse)
+        return palindromeArray.every((element,index) => element === palidromReverse[index])
+    }
 
 // Do not edit below this line
 module.exports = palindromes;
 
+//Старый вариант 
+// const palindromes = function (string) {
+//     const palindromeArray = string.trim().replace(/[^a-z]/ig, '').toLowerCase().split('');
+//     const halfArray = Math.floor(palindromeArray.length / 2);
+//     const lastIndex = palindromeArray.length - 1;    
 
+//         // console.log(palindromeArray,halfArray,lastIndex)
+//         for (let i = 0; i < halfArray; i++ ){
+//             if (palindromeArray[i] !== palindromeArray[lastIndex-i]){
+//                 return false
+//             }
+//         }
+//     return true
+// };
 //Для поиска в словах
 // const palindromes = function (string) {
 //     const palindromeArray = string.replace(/[!%$]/g, '').toLowerCase().split(' ');
